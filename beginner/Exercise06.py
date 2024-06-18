4a24bce6035d964e6498e3b2a36aa283762c5621
Arretados Labs <arretadolabs@gmail.com>
2024-06-18 15:33:32 -0300

    New Exercise of calculate of area circle

---
+++ beginner/Exercise06.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+"""
+
+# Section of variable with data user input
+PI = 3.14  # Variable with value constant
+raid_circle = float(input("What value of raid circle: "))
+
+# Calculating area of circle
+print("The area of circle is: ", PI * (raid_circle**2))
