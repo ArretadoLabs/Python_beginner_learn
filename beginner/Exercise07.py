7ba5f7a29c815553ca0ffe733fc7a9b64f43f12f
Arretados Labs <arretadolabs@gmail.com>
2024-06-18 15:42:51 -0300

    calculate value of area square

---
+++ beginner/Exercise07.py
@@ -0,0 +1,15 @@
+"""
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+"""
+
+# section of variables with data user input
+side_square = float(input("What value of side square: "))
+
+# calculating area of square
+area_square = side_square**2
+
+# Printing value final of square area
+print("Area of square is: ", area_square)
+
+# Now printing value of area square double
+print("The value double area of square is: ", area_square * 2)
