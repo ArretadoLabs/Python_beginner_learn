76c4fc40afa4e92594e931dcb8f5e618af6ac371
Arretados Labs <arretadolabs@gmail.com>
2024-06-19 08:49:32 -0300

    Remove lines and code clean

--- beginner/Exercise15.py
+++ beginner/Exercise15.py
@@ -13,19 +13,14 @@ hours_worked_month = int(input("How many you worked in month: "))
 
 # Calculating values, logical structure
 salary_brute = hour_work_value * hours_worked_month
-inss = (salary_brute * 0.08)
-ir = (salary_brute * 0.11)
-sindicate = (salary_brute * 0.05)
+inss = salary_brute * 0.08
+ir = salary_brute * 0.11
+sindicate = salary_brute * 0.05
 salary_liquid = salary_brute - (inss + ir + sindicate)
 
-#Display values 
+# Display values
 print("You salary brute is: ", salary_brute)
 print("You value discount under INSS: ", inss)
 print("You value discount under IR: ", ir)
 print("You value discount under sindicate: ", sindicate)
 print("And you salary liquid is: R$ ", salary_liquid)
-
-
-
-
-
